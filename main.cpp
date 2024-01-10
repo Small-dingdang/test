8120df43cd3cdd8721a1bff8e666a1bd1f836147
qiaokai <qiaokai>
2024-01-10 09:54:56 +0800

    fix

--- main.cpp
+++ main.cpp
@@ -1,16 +1,9 @@
 #include<iostream>
+#include "server.h"
 using namespace std;
-
-extern void foo();
 
 int main()
 {
-	cout << "Hello, world!" << endl;
-	cout << "Hello, world!" << endl;
-	cout << "Hello, world!" << endl;
-	cout << "Hello, world!" << endl;
-	cout << "Hello, world!" << endl;
-	foo();
 	return 0;
 }
 
